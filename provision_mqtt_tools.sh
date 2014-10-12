c2ea97613e658d58bc6bd43249b2eef42d632f06
Zvi Avraham <zvi@nivertech.com>
2014-10-12 22:27:35 +0300

    upgrade mosquitto to v1.3.5

--- provision_mqtt_tools.sh
+++ provision_mqtt_tools.sh
@@ -20,16 +20,27 @@ tar xzf master.tar.gz
 rm master.tar.gz
 cd mqtt-sn-tools-master/
 make
-echo "export PATH=$PATH:$HOME_DIR/mqtt-sn-tools-master/" >> $HOME_DIR/.bashrc
+echo "export PATH=\$PATH:$HOME_DIR/mqtt-sn-tools-master/" >> $HOME_DIR/.bashrc
 
 # install mosquitto fro it's mosquitto_pub/sub CLI tools
 sudo apt-get install -y build-essential libtool m4 autoconf openssl libssl-dev libc-ares-dev
 cd $HOME_DIR
-wget http://mosquitto.org/files/source/mosquitto-1.3.2.tar.gz
-tar xzf mosquitto-1.3.2.tar.gz
-rm mosquitto-1.3.2.tar.gz
-cd mosquitto-1.3.2
+wget http://mosquitto.org/files/source/mosquitto-1.3.5.tar.gz
+tar xzf mosquitto-1.3.5.tar.gz
+rm mosquitto-1.3.5.tar.gz
+cd mosquitto-1.3.5
 make
 sudo make install
-rm -rf mosquitto-1.3.2/
-echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib" >> $HOME_DIR/.bashrc
+rm -rf mosquitto-1.3.5/
+echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/usr/local/lib" >> $HOME_DIR/.bashrc
+
+# TODO - git should be configured on host machine
+# install RSMB from source
+cd $HOME_DIR
+git clone git://git.eclipse.org/gitroot/mosquitto/org.eclipse.mosquitto.rsmb.git
+cd org.eclipse.mosquitto.rsmb/
+cd rsmb/src/
+make
+cd ../../amqtdd/src/
+make
+echo "export PATH=\$PATH:$HOME_DIR/rsmb/src/:$HOME_DIR/amqtdd/src/" >> $HOME_DIR/.bashrc
